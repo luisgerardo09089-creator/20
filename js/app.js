b135287a73a6647bade9635883346114a2017352
luisgerardo09089-creator <luisgerardo09.089@gmail.com>
2026-04-20 18:46:25 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
 function mostrarFlores() {
+    const flores = document.querySelectorAll(".flor");
+
+    flores.forEach((flor, index) => {
+        setTimeout(() => {
+            flor.style.opacity = "1";
+            flor.style.transform = "translateY(0)";
+        }, index * 150);
+    });
+}function mostrarFlores() {
     const jardin = document.getElementById("jardin");
     jardin.classList.remove("oculto");
 }
