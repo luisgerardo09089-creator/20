7abb48c4ebdb5a5f93cc58219ad392406f04a4df
luisgerardo09089-creator <luisgerardo09.089@gmail.com>
2026-04-20 18:55:15 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,8 +3,8 @@ function mostrarFlores() {
 
     flores.forEach((flor, index) => {
         setTimeout(() => {
-            flor.style.opacity = "1";
-            flor.style.transform = "translateY(0)";
+            flor.estyles.opacity = "1";
+            flor.estyles.transform = "translateY(0)";
         }, index * 150);
     });
 }
